39bd4b92eac29e2df891e980f39bf9b0b39906a7
Sneh Jani <snehmegh3006@gmail.com>
2023-03-03 00:52:28 +0530

    This is after a while

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Basic from "./components/Basic"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mantrin</h1>
+      <Basic />
+    </div>
+  );
+}
+
+export default App;
