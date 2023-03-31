4bf6e3658057515163dc2a4a035123a62b614de2
Sneh Jani <snehmegh3006@gmail.com>
2023-03-31 23:32:45 +0530

    simple f up

--- actions/actions.py
+++ actions/actions.py
@@ -1,33 +1,33 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+# # This files contains your custom actions which can be used to run
+# # custom Python code.
+# #
+# # See this guide on how to implement these action:
+# # https://rasa.com/docs/rasa/custom-actions
 
 
-# This is a simple example for a custom action which utters "Hello World!"
-import mysql.connector as mysql
+# # This is a simple example for a custom action which utters "Hello World!"
+# import mysql.connector as mysql
 
-from typing import Any, Text, Dict, List
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
-from rasa_core_sdk.events import SlotSet
-from rasa_core_sdk import Tracker
-import requests
-import random
+# from typing import Any, Text, Dict, List
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+# from rasa_core_sdk.events import SlotSet
+# from rasa_core_sdk import Tracker
+# import requests
+# import random
 
-conn= mysql.connect(user='root',password ='root',host='localhost',database='bot_data')
-cursor = conn.cursor()
+# conn= mysql.connect(user='root',password ='root',host='localhost',database='bot_data')
+# cursor = conn.cursor()
 
-class ActionHelloWorld(Action):
+# class ActionHelloWorld(Action):
 
-    def name(self) -> Text:
-        return "action_hello_world"
+#     def name(self) -> Text:
+#         return "action_hello_world"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        dispatcher.utter_message(text="Hello World!")
+#         dispatcher.utter_message(text="Hello World!")
 
-        return []
+#         return []
